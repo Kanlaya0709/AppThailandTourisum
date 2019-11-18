23fd6f74cf07a542fc6878b58609f630d5aa7129
Kanlaya0709 <59160539@go.buu.ac.th>
2019-11-19 01:16:18 +0700

    logging oncreate&onstart

--- app/src/main/java/com/example/thailandtourisum/MainActivity.kt
+++ app/src/main/java/com/example/thailandtourisum/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.thailandtourisum
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i("MainActivity","onCreate called")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("MainActivity","onStart called")
+
     }
 }
